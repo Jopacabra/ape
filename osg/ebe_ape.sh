f146d7d20a35f8897c86622fb2b86627e4047d25
jo <joseph.bahder@gmail.com>
2025-11-21 15:07:19 +0800

    More flexible file handling in OSG bash script

--- osg/ebe_ape.sh
+++ osg/ebe_ape.sh
@@ -3,7 +3,9 @@
 printf "Start time: "; /bin/date
 printf "Job is running on node: "; /bin/hostname
 printf "Job running as user: "; /usr/bin/id
-printf "Job is running in directory: "; /bin/pwd  # Should be /srv/scratch by default
+work_dir=$(pwd)
+printf "Job is running in directory: "
+echo $work_dir  # Should be /srv/scratch by default
 echo
 
 #set -x  # Enable debugging
@@ -22,9 +24,10 @@ echo "Event complete!"
 
 # Transfer relevant output files to /srv
 echo "Moving results parquet to /srv..."
-mv /srv/results/*/*.pickle /srv
-mv /srv/results/*/*.dat /srv
-mv /srv/results/*/*.npy /srv
-mv /srv/results/*/*.nc /srv
+mv $work_dir/results/*/*.pickle $work_dir
+mv $work_dir/results/*/*.dat $work_dir
+mv $work_dir/results/*/*.npy $work_dir
+mv $work_dir/results/*/*.nc $work_dir
+mv $work_dir/results/*/*.log $work_dir
 
 echo "Job complete! Have a great day! :)"
