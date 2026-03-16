71b5b8414538088e0e97803c0713bde41f1c13b4
jo <joseph.bahder@gmail.com>
2026-03-16 14:36:11 +0800

    Add z gradients of temperture & transverse plane flow -- 0 in longitudinally boost invariant approximation.

--- plasma.py
+++ plasma.py
@@ -440,10 +440,13 @@ class plasma_event:
     z_vel: Callable[[Any], np.ndarray] = field(init=False)
     temp_grad_x: Callable[[Any], np.ndarray] = field(init=False)
     temp_grad_y: Callable[[Any], np.ndarray] = field(init=False)
+    temp_grad_z: Callable[[Any], np.ndarray] = field(init=False)
     grad_x_u_x: Callable[[Any], np.ndarray] = field(init=False)
     grad_x_u_y: Callable[[Any], np.ndarray] = field(init=False)
     grad_y_u_x: Callable[[Any], np.ndarray] = field(init=False)
     grad_y_u_y: Callable[[Any], np.ndarray] = field(init=False)
+    grad_z_u_x: Callable[[Any], np.ndarray] = field(init=False)
+    grad_z_u_y: Callable[[Any], np.ndarray] = field(init=False)
 
     # Domain metadata (set in __post_init__)
     timestep: float = field(init=False)
@@ -493,10 +496,13 @@ class plasma_event:
         self.z_vel = (lambda x : np.tanh(x[3]))
         self.temp_grad_x = _BoostInvariantMilneAdapter(gradxT_3d) if gradxT_3d is not None else _BoostInvariantMilneAdapter(lambda p: np.zeros(p.shape[:-1]))
         self.temp_grad_y = _BoostInvariantMilneAdapter(gradyT_3d) if gradyT_3d is not None else _BoostInvariantMilneAdapter(lambda p: np.zeros(p.shape[:-1]))
+        self.temp_grad_z = _BoostInvariantMilneAdapter(lambda x : 0)
         self.grad_x_u_x = _BoostInvariantMilneAdapter(grad_xux_3d) if grad_xux_3d is not None else _BoostInvariantMilneAdapter(lambda p: np.zeros(p.shape[:-1]))
         self.grad_x_u_y = _BoostInvariantMilneAdapter(grad_xuy_3d) if grad_xuy_3d is not None else _BoostInvariantMilneAdapter(lambda p: np.zeros(p.shape[:-1]))
         self.grad_y_u_x = _BoostInvariantMilneAdapter(grad_yux_3d) if grad_yux_3d is not None else _BoostInvariantMilneAdapter(lambda p: np.zeros(p.shape[:-1]))
         self.grad_y_u_y = _BoostInvariantMilneAdapter(grad_yuy_3d) if grad_yuy_3d is not None else _BoostInvariantMilneAdapter(lambda p: np.zeros(p.shape[:-1]))
+        self.grad_z_u_x = _BoostInvariantMilneAdapter(lambda x: 0)
+        self.grad_z_u_y = _BoostInvariantMilneAdapter(lambda x: 0)
 
         # Set domains from the underlying interpolator grid when available
         try:
