Fix integral over energy distribution & time printout for radiation computation.

parton_evolution.py
@@ -119,6 +119,8 @@ def evolve_particle(particle : hard_particles.Particle, plasma_object : plasma.p
                                                         num_k_perp_points // 2)
                         k_perp_values = np.concatenate((-np.flip(k_perp_pos_values[1:]), k_perp_pos_values))
 
+                        _, _, x_grid = np.meshgrid(k_perp_values, k_perp_values, x_values, indexing='ij')
+
                         emission_momenta = []
 
                         # Compute radiation distribution from this step -- returned in (kx, ky, kz) in parton frame
@@ -129,7 +131,6 @@ def evolve_particle(particle : hard_particles.Particle, plasma_object : plasma.p
 
                         # Compute integral of complete radiation distribution
                         # (np.trapezoid handles integration of arbitrary spacing via coordinates)
-                        t0 = time.time()
                         if fixed_norm:
                             # Fix analytic expectation for gluon emissions per step
                             total_number = pi.N_gluons(particle=particle, medium=plasma_object, dtau=dtau)
@@ -142,7 +143,7 @@ def evolve_particle(particle : hard_particles.Particle, plasma_object : plasma.p
                                                           k_perp_values, axis=0)  # Integrate over kx -> scalar
                             total_energy += np.trapezoid(
                                 np.trapezoid(
-                                    np.trapezoid(dtau_rad_dist * x_values * particle.E0, k_values, axis=2),  # Integrate over kz -> shape: (n_kx, n_ky)
+                                    np.trapezoid(dtau_rad_dist * x_grid * particle.E0, k_values, axis=2),  # Integrate over kz -> shape: (n_kx, n_ky)
                                                           k_perp_values, axis=1),  # Integrate over ky -> shape: (n_kx)
                                                           k_perp_values, axis=0)  # Integrate over kx -> scalar
                             logging.debug(f"Radiation number distribution integral: {total_number}")
@@ -156,7 +157,7 @@ def evolve_particle(particle : hard_particles.Particle, plasma_object : plasma.p
                             # Sample the distribution for emission kinematics in the radiation frame
                             k = plasma_interaction.sample_rad_dist(dtau_rad_dist, N_samples=1,
                                                                    kx_values=k_perp_values, ky_values=k_perp_values,
-                                                                   kz_values=k_values)
+                                                                   kz_values=k_values, mu=mu, E=particle.E0)
                             logging.debug(f"Emitting gluon! Radiation frame info:")
 
                             # If we rescale energies, do it!

plasma_interaction.py
@@ -952,7 +952,7 @@ def N_gluons_fk(particle: hard_particles.Particle, medium: plasma.plasma, dtau: 
     return CR * np.trapezoid(y=int_vals, x=x_vals)
 
 
-def sample_rad_dist(rad_dist, kx_values, ky_values, kz_values, N_samples=1):
+def sample_rad_dist(rad_dist, kx_values, ky_values, kz_values, N_samples=1, kin_cut=True, mu=0.3, E=10):
     """
     Function to sample radiation distribution for kx, ky, kz.
     Treat dI/(dxdkxdky) as an (unnormalized) 3D probability density and draw N_samples points (kx, ky, kz) from it.
@@ -973,17 +973,38 @@ def sample_rad_dist(rad_dist, kx_values, ky_values, kz_values, N_samples=1):
     cdf = np.cumsum(pdf)  # build CDF
     cdf[-1] = 1.0  # Force exact upper bound — removes all floating point slop
 
-    # Draw uniform samples and find where they land in the CDF
-    uniform_samples = 1.0 - rng.uniform(size=N_samples)
-    flat_indices = np.searchsorted(cdf, uniform_samples, side="right")  # shape: (N_samples,)
+    while True:
+        # Draw uniform samples and find where they land in the CDF
+        uniform_samples = 1.0 - rng.uniform(size=N_samples)
+        flat_indices = np.searchsorted(cdf, uniform_samples, side="right")  # shape: (N_samples,)
 
-    # Convert flat indices back to 3D grid indices
-    ikx, iky, ikz = np.unravel_index(flat_indices, rad_dist.shape)
+        # Convert flat indices back to 3D grid indices
+        ikx, iky, ikz = np.unravel_index(flat_indices, rad_dist.shape)
 
-    # Look up the corresponding coordinate values and add jitter about the bin, so we don't sample exactly on the points
-    sampled_kx = kx_values[ikx] + rng.uniform(-0.5, 0.5, size=N_samples) * dkx[ikx]
-    sampled_ky = ky_values[iky] + rng.uniform(-0.5, 0.5, size=N_samples) * dky[iky]
-    sampled_kz = kz_values[ikz] + rng.uniform(-0.5, 0.5, size=N_samples) * dkz[ikz]
+        # Look up the corresponding coordinate values and add jitter about the bin, so we don't sample exactly on the points
+        sampled_kx = kx_values[ikx] + rng.uniform(-0.5, 0.5, size=N_samples) * dkx[ikx]
+        sampled_ky = ky_values[iky] + rng.uniform(-0.5, 0.5, size=N_samples) * dky[iky]
+        sampled_kz = kz_values[ikz] + rng.uniform(-0.5, 0.5, size=N_samples) * dkz[ikz]
+
+        if kin_cut:
+            failed = False
+            # Check if the sampled point is within the kinematic cuts
+            for kx, ky, kz in zip(sampled_kx, sampled_ky, sampled_kz):
+                kperp2 = (kx**2 + ky**2)
+                x = kz / E
+                if ((kperp2 >= mu**2) and (kperp2 < (4*(E**2) * min(x**2, (1-x)**2) - mu**2)) and (x > mu/(2*E))
+                        and (x < 1 - mu/(2*E))):
+                    pass  # Success
+                else:
+                    failed = True
+                    break
+            if failed:
+                continue  # Sample another set.
+            break  # Successful cut!
+
+
+        else:
+            break
 
     # Stack values
     emission_momentum = np.column_stack([sampled_kx, sampled_ky, sampled_kz])