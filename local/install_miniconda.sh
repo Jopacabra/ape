76318db791e856e9083f4b984f4ae2806bf47d2d
jopacabra <joseph.bahder@gmail.com>
2026-09-03 12:42:52 +0800

    Separate miniconda install from prereq install.

---
+++ local/install_miniconda.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# For a local install, run this script first. Run this script without elevated permissions
+# (e.g. bash install_miniconda.sh)
+
+# Go to home
+cd ~
+
+# Get & install Miniconda
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+bash Miniconda3-latest-Linux-x86_64.sh
--- local/ubuntu_prereqs.sh
+++ local/ubuntu_prereqs.sh
@@ -2,9 +2,6 @@
 # For a local install, run this script first. Run this script with elevated permissions
 # (e.g. sudo bash ubuntu_prereqs.sh)
 
-# Go to home
-cd ~
-
 # Update apt-get stuff
 sudo apt-get -y update
 
@@ -13,7 +10,3 @@ sudo apt-get -y install build-essential libtool autoconf unzip wget
 sudo apt-get -y install cmake
 sudo apt-get -y install libboost-all-dev
 sudo apt-get -y install libhdf5-serial-dev
-
-# Get Conda
-wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
-bash Miniconda3-latest-Linux-x86_64.sh
